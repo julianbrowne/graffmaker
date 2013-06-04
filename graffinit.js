a4795e3135b16e05fb8066b6980dafee964536f1
Julian Browne <julianbrowne@gmail.com>
2013-06-04 18:55:34 +0100

    model builder

--- graffinit.js
+++ graffinit.js
@@ -1,59 +1,63 @@
 
-var neo   = require('neo4j');
-var mw    = require('./lib/fixtures');
-var utils = require('./lib/utils');
-var obj   = require('./data');
+var model = require('./lib/models').actions;
+var type  = require('./lib/data').types;
+var util  = require('util');
+var seq   = require('./lib/step');
 
-var db = new neo.GraphDatabase(process.env.NEO4J_URL || 'http://localhost:7474');
-
-/**
- *  Add nodes to Neo
-**/
-
-lgr = function(e,n) {
-    if(e)
-        throw(e);
-    else {
-        console.log(n.data);
-
-        n.index('nodeIndex', 'name', n.data.name);
-
-        var cypher = [
-            "START n=node(*)",
-            "RETURN n;"
-        ].join("\n");
-
-        db.query(cypher, {}, function(e, r) {
-            var items = [];
-            if(r) {
-                r.map(function(i){ items.push(i['n']); });
-                console.log('node count => ' + items.length);
-            }
-        });
+function linkFrom(nodeTarget, relationship, cb) {
+    return function(nodeSource) {
+        model.linkNodes(nodeTarget, nodeSource, relationship);
+        if(cb!==undefined) cb(nodeSource);
     }
-};
+}
 
-db.createNode(new obj.model.Person("John",  "Smith")).save(lgr);
-db.createNode(new obj.model.Person("Brian", "Jones")).save(lgr);
+function linkTo(nodeTarget, relationship, cb) {
+    return function(nodeSource) {
+        model.linkNodes(nodeSource, nodeTarget, relationship);
+        if(cb!==undefined) cb(nodeSource);
+    }
+}
 
-db.createNode(new obj.model.Home("Home")).save(lgr);
-db.createNode(new obj.model.Home("Home")).save(lgr);
-db.createNode(new obj.model.Home("Home")).save(lgr);
+var builder = model.builder();
 
-db.createNode(new obj.model.Room("Living Room")).save(lgr);
-db.createNode(new obj.model.Room("Kitchen")).save(lgr);
-db.createNode(new obj.model.Room("Bedroom")).save(lgr);
+seq.step(
 
-db.createNode(new obj.model.Room("Living Room")).save(lgr);
-db.createNode(new obj.model.Room("Study")).save(lgr);
+    function() {
+        model.builder().make(type.Person).using("John","Smith").create(this);
+    },
 
-db.createNode(new obj.model.Room("Living Room")).save(lgr);
-db.createNode(new obj.model.Room("Dining Room")).save(lgr);
+    function(person) {
+        model.builder().make(type.Gas).using("Gas #231").create(linkFrom(person, 'holds-account'));
+        model.builder().make(type.Home).using("Home 1").create(linkFrom(person, 'owns-property', this));
+    },
 
-db.createNode(new obj.model.Gas("Gas Acc.")).save(lgr);
-db.createNode(new obj.model.Electricity("Elec. Acc.")).save(lgr);
-db.createNode(new obj.model.DuelFuel("Duel Fuel")).save(lgr);
+    function(property) {
+        builder.make(type.Room).using("Living Room").create(linkFrom(property, 'contains', this));
+        builder.make(type.Room).using("Kitchen").create(linkFrom(property, 'contains'));
+        builder.make(type.Room).using("Main Bedroom").create(linkFrom(property, 'contains'));
+        builder.make(type.Room).using("Nursery").create(linkFrom(property, 'contains'));
+        builder.make(type.Room).using("Study").create(linkFrom(property, 'contains'));
+    },
 
-db.createNode(new obj.model.Device("Thermostat", "AlertMe", "Emporer")).save(lgr);
-db.createNode(new obj.model.Device("Thermostat", "Nest", "3.5")).save(lgr);
-db.createNode(new obj.model.Device("Humidity", "XBee", "ZB")).save(lgr);
+    function(room) {
+        builder.make(type.Device).using("Thermostat", "AlertMe", "Emporer").create(linkTo(room, 'installed-in'));
+    }
+);
+
+seq.step(
+    function() { model.createNode(new type.Person("Brian", "Jones"), this); },
+    function(person) {
+        model.createNode(new type.Gas("Elec #652"), linkFrom(person, 'holds-account'));
+        model.createNode(new type.Home("Flat 2B"), linkFrom(person, 'owns-property', this));
+    },
+    function(property) {
+        model.createNode(new type.Room("Hall"), linkFrom(property, 'contains'));
+        model.createNode(new type.Room("Kitchen"), linkFrom(property, 'contains'));
+        model.createNode(new type.Room("Master Bedroom"), linkFrom(property, 'contains'));
+        model.createNode(new type.Room("Spare Bedroom"), linkFrom(property, 'contains'));
+        model.createNode(new type.Room("Dining Room"), linkFrom(property, 'contains', this));
+    },
+    function(room) {
+        model.createNode(new type.Device("Thermostat", "Nest", "3.5"), linkTo(room, 'installed-in'));
+    }
+);
