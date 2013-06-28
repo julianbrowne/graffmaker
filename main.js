cb1bae42c203dd8b5aa52ddfd6869dd07bfb7f72
Julian Browne <julianbrowne@gmail.com>
2013-06-28 13:30:25 +0100

    connect function

--- main.js
+++ main.js
@@ -5,8 +5,10 @@
 
 (function() {
 
-  var model = require('./lib/model').actions;
+  var model   = require('./lib/model').actions;
+  var connect = require('./lib/model').connect;
 
+  exports.connect     = connect;
   exports.createNode  = model.createNode;
   exports.connectTo   = model.connectTo;
   exports.connectFrom = model.connectFrom;
