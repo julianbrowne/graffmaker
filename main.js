5048cd061c6a81a6ab00b99bebef847dfa0b0675
Julian Browne <julianbrowne@gmail.com>
2013-06-28 14:44:01 +0100

    connect option

--- main.js
+++ main.js
@@ -5,13 +5,6 @@
 
 (function() {
 
-  var model   = require('./lib/model').actions;
-  var connect = require('./lib/model').connect;
-
-  exports.connect     = connect;
-  exports.createNode  = model.createNode;
-  exports.connectTo   = model.connectTo;
-  exports.connectFrom = model.connectFrom;
-  exports.define      = model.define;
+  exports.connect = require('./lib/model').connect;
 
 }).call(this);
